ef0f0eb73fc75b8a66a5188cc4100b31e4f6b5e5
homuya789 <dddzae04@gmail.com>
2026-02-28 02:23:03 +0900

    update

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+//SafeArea 사용해서 노치, 홈인디케이터 영역 피하기
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -8,11 +10,23 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
+class WriteBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.all(16),
+      color: Colors.white,
+      child: Text("글을 작성해보세요"),
+    );
+  }
+}
+
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 232, 227, 220),
+      backgroundColor: const Color.fromRGBO(248, 246, 242, 1),
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 255, 255, 255),
         title: Column(
@@ -34,13 +48,14 @@ class _HomeState extends State<Home> {
                 color: Color(0xFF9C8F84),
               ),
             ),
+            SizedBox(height: 6),
           ],
         ),
         bottom: PreferredSize(
           preferredSize: const Size.fromHeight(1),
           child: Container(
-            height: 3,
-            color: const Color(0xFFE0D6CC), //AppBar 아래 선
+            height: 2,
+            color: const Color.fromRGBO(232, 227, 220, 1), //AppBar 아래 선
           ),
         ),
         actions: [
@@ -73,42 +88,29 @@ class _HomeState extends State<Home> {
             onPressed: () {},
           ),
         ],
-        /*
-        
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: Text.rich(
-              TextSpan(
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w800,
-                  color: Color(0xFFC8956A),
-                ),
-                children: const [
-                  TextSpan(text: "지금 2,000명과 함께해요."),
-                  TextSpan(
-                    text: " 텍스트",
-                    style: TextStyle(color: Color(0xFF3D2E26)),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],*/
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(60),
+
+      body: Padding(
+        padding: EdgeInsets.only(left: 60, right: 60, top: 20, bottom: 40),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               "TODAY",
               style: TextStyle(
-                fontSize: 14,
+                fontSize: 10,
                 color: Color.fromARGB(200, 149, 106, 1),
               ),
             ),
+            Text(
+              '"지쳐도 괜찮아요. 오늘 하루를 살아낸 것만으로도 충분합니다."',
+              style: GoogleFonts.nanumMyeongjo(
+                fontSize: 13,
+                color: Color(0xFF3D2E26),
+              ),
+            ),
+            SizedBox(height: 20),
+            WriteBox(),
           ],
         ),
       ),
